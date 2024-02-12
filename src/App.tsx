bd4f61f8e858ddea8a5052374e823c7dac550872
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2024-02-12 16:46:31 -0300

    Added radix-ui, date-fns and lucide packages, styled modal on click on note

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard, NoteCard } from './components'
 
+const note = {
+    date: new Date(),
+    content: '123',
+}
+
 export function App() {
     return (
         <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -17,10 +22,7 @@ export function App() {
 
             <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
                 <NewNoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
+                <NoteCard note={note} />
             </div>
         </div>
     )
